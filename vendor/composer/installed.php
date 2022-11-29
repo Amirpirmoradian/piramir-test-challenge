8e79e266ea4239cc28f42213f3bf630f35142a69
Amir Pirmoradian <piramir77@gmail.com>
2022-11-29 18:23:14 +0330

    add composer to use autoload feature

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'piramir/piramir-challenge-test',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '9499788fd264074d2644cf8583fda62adb3b7005',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'piramir/piramir-challenge-test' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '9499788fd264074d2644cf8583fda62adb3b7005',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
