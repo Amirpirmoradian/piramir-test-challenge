59e82e37cdbcbdbe839337eb9b60e8667c2516f7
Amir Pirmoradian <piramir77@gmail.com>
2022-11-29 18:31:44 +0330

    Add empty uninstall.php file

---
+++ uninstall.php
@@ -0,0 +1,11 @@
+<?php
+if(! function_exists('add_action')){
+	die();
+}
+
+//TODO remove all custom posts and taxonomies and any change to database
+
+//
+//global $wpdb;
+//
+$wpdb->query('DELETE FROM '.$wpdb->prefix.'posts WHERE');
