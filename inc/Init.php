ef1daa40fcffa7e754055f7f0007e8173e41498f
Amir Pirmoradian <piramir77@gmail.com>
2022-11-30 16:30:43 +0330

    improve Init readability

--- inc/Init.php
+++ inc/Init.php
@@ -7,6 +7,15 @@
 namespace Piramir;
 
 
+use Piramir\Base\Enqueue;
+use Piramir\Base\Language;
+use Piramir\CustomMetaBox\ExtraInfo;
+use Piramir\CustomPostType\Movie;
+use Piramir\CustomTaxonomy\Genre;
+use Piramir\CustomTaxonomy\Tag;
+use Piramir\Shortcode\MovieList;
+use Piramir\Template\SingleMovie;
+use Piramir\Widget\MoviesCount;
 
 final class Init {
 
@@ -15,17 +24,46 @@ final class Init {
 	 * @return string[]
 	 */
 	public static function get_services(): array {
-		return [
-			Base\Enqueue::class,
-			Base\Language::class,
-			CustomPostType\Movie::class,
-			CustomTaxonomy\Genre::class,
-			CustomTaxonomy\Tag::class,
-			CustomMetaBox\ExtraInfo::class,
-			Template\SingleMovie::class,
-			Shortcode\MovieList::class,
-			Widget\MoviesCount::class,
+
+		$base = [
+			Enqueue::class,
+			Language::class
 		];
+
+		$custom_post_types = [
+			Movie::class,
+		];
+
+		$custom_taxonomies = [
+			Genre::class,
+			Tag::class
+		];
+
+		$custom_meta_boxes = [
+			ExtraInfo::class
+		];
+
+		$custom_templates = [
+			SingleMovie::class
+		];
+
+		$custom_shortcodes = [
+			MovieList::class
+		];
+
+		$custom_widgets = [
+			MoviesCount::class
+		];
+
+		return array_merge(
+			$base,
+			$custom_post_types,
+			$custom_taxonomies,
+			$custom_meta_boxes,
+			$custom_templates,
+			$custom_shortcodes,
+			$custom_widgets
+		);
 	}
 
 	/**
